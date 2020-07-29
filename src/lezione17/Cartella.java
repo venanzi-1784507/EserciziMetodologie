04ded78c91143f1a692f4431ed2bf0253f63539a
venanzi-1784507 <venanzi.1784507@studenti.uniroma1.it>
2020-07-30 01:25:31 +0200

    add research in a directory method

--- src/lezione17/Cartella.java
+++ src/lezione17/Cartella.java
@@ -1,6 +1,8 @@
 package lezione17;
 
 import java.io.File;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Cartella {
 	
@@ -28,6 +30,38 @@ public class Cartella {
 		return null;
 	}
 	
+	public static List<File> cercaPerEstensioni(File dir, String[] extensions){
+		List<File> out = new ArrayList<>();
+		for(String s: extensions)
+			out.addAll(cercaPerEstensione(dir, s));
+		return out;
+	}
+	
+	public static List<File> cercaPerEstensione(File dir, String extension){
+		List<File> out = new ArrayList<>();
+		return cercaRicorsivaPerEstensione(dir, extension, out);
+	}
+	
+	private static List<File> cercaRicorsivaPerEstensione(File file, String extension, List<File> data){
+		//caso base
+		if(file.isFile())
+			return data;
+		
+		//allora è una directory
+		for(File f: file.listFiles()) {
+			//se è un file controllo l'estensione e se è quella che cerco lo inserisco nella lista
+			if(f.isFile()) {
+				if(f.getName().substring(f.getName().indexOf(".")+1).equals(extension))
+					data.add(f);
+			}
+			else {
+				//allora è una directory
+				data = cercaRicorsivaPerEstensione(f, extension, data);
+			}
+		}
+		return data;
+	}
+	
 	public static void main(String[] args) {
 		File f = new File("/home/giaco/Documenti/prova_java");
 		//System.out.println(f.getName().substring(0, f.getName().indexOf('.')));
@@ -37,6 +71,15 @@ public class Cartella {
 		
 		f1 = cercaFile(f, "file1");
 		System.out.println(f1.getAbsolutePath());
+		
+		
+		//System.out.println(cercaPerEstensione(f, "txt").toString());
+		System.out.println("[stampa per estensione]");
+		cercaPerEstensione(f, "txt").forEach(x->System.out.println(x.getName()));
+		
+		System.out.println("[stampa per estensioni]");
+		String[] array = {"txt", "jpeg", "png"};
+		cercaPerEstensioni(f, array).forEach(x->System.out.println(x.getName()));;
 	}
 
 }
